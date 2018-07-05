4a21c69776034899e3a99807cff53f2e7bd016f6
Evan Kriminger <evankriminger@gmail.com>
2018-07-05 03:06:11 -0700

    primary key matching working

--- README.md
+++ README.md
@@ -1,20 +1,31 @@
 ### Fast CSV Chunker
 
-Assumes first row contains the headers. No delimiter type needed... but will break if you have endline symbols as entries. 
+This is a tool written in C++11 to split CSV files too large for memory into chunks with a specified number of rows.
+
+It is often undesirable to have rows with the same primary key value in different shards. For example, how would we do a join in that case? This tool can optionally ensure that all rows with the same primary key value end up in the same file. This mode is triggered when a primary key is specified in the command line arguments.
+
+Grouping matching keys has its drawbacks. The task of parsing the line for the key value, adds computation. In addition, the resulting chunks will no longer have the specified number of rows, as rows might be added to any of the existing files, even after we have moved on to a new chunk. 
+
+Assumptions:
+- First row contains the headers
+- Delimiter is ',' (TODO)
 
 TODO:
-  - Specify primary key, and keep all rows with the same key in the same shard.
+- Named arg parsing and add delimiter
+- Check for quotes (just doing a simple delimiter count now)
 
 #### Build
 
+Only dependencies are in the C++ standard library. 
 ```
 g++ -std=c++11 -o chunker main.cc
 ```
 
 #### Usage
 
+__Case 1__: Simple chunking
 ```
-./chunker <csv_file> <rows_per_chunk>
+./chunker <csv_file> <rows_per_chunk> 
 ```
 
 Output:
@@ -26,3 +37,17 @@ Output:
   ├── ...
   └── chunk_000049.csv
 ```
+
+__Case 2__: Chunk with primary key groupings
+
+```
+./chunker <csv_file> <rows_per_chunk> <primary_key>
+```
+
+For example, to break the Iris dataset into a separate .csv file for each class, run:
+
+```
+./chunker iris.csv 10 species
+```
+
+The primary key is `species`, so for each row, the species is parsed, and the row is added to the file where the first instance of that species was added.
--- main.cc
+++ main.cc
@@ -3,6 +3,8 @@
 #include <vector>
 #include <string>
 #include <typeinfo>
+#include <memory>
+#include <map>
 
 using std::cout;
 using std::endl;
@@ -10,8 +12,9 @@ using std::vector;
 using std::string;
 
 
-string HORZ_LINE(50, '-');
-char CHUNK_PREF[] = "chunk_";
+const string HORZ_LINE(50, '-');
+const char CHUNK_PREF[] = "chunk_";
+
 
 void readme(){
   cout << "./chunk_csv <csv_file> <rows_per_chunk> <primary_key(opt)>" << endl;
@@ -40,16 +43,17 @@ Returns:
 unsigned parse_cols(vector<string>& col, string& all_cols, string& primary_key){
   size_t start = 0;
   size_t L_cnt = 0;
-  unsigned col_cnt = 0;
+  unsigned delim_cnt = 0;
   int col_idx = -1;
   for (int i = 0; i < all_cols.length(); ++i){
-    if (all_cols[i] == ','){
+    if (all_cols[i] == ',' || i == all_cols.length()-1){
+      L_cnt = i == all_cols.length()-1 ? L_cnt + 1 : L_cnt;
       string col_name(all_cols, start, L_cnt);
       if ( col_name.compare(primary_key) == 0){
-        col_idx = col_cnt;
+        col_idx = delim_cnt;
       }
       col.push_back(col_name);
-      ++col_cnt;
+      ++delim_cnt;
       L_cnt = 0;
       start = i + 1;
     } else {
@@ -108,28 +112,62 @@ int main(int argc, char **argv){
 
   unsigned row_cnt = 0;
   unsigned chunk_cnt = 0;
+  bool make_new_chunk = true;
   char output_fn[50];
-  std::ofstream output_file;
+  //vector<std::unique_ptr<std::ofstream>> output_file_vec;
+  vector<std::ofstream*> output_file_vec;
+
+  std::map<string, unsigned> keys_to_chunks;
+  string this_key;
 
   while( getline(input_file, line) ){
-    if (row_cnt == 0){
+    if (make_new_chunk){
       sprintf(output_fn, "%s%06d.csv", CHUNK_PREF, chunk_cnt);
       cout << output_fn << endl;
-      output_file.open(output_fn);
-      output_file << header << endl;
-    }
-   
-    output_file << line << endl;
 
-    if (row_cnt == rows_per_chunk-1){
+      // making new ofstream that will persist
+      std::ofstream *p_new_ofstream = new std::ofstream(output_fn);
+      
+      output_file_vec.push_back(p_new_ofstream);
+   
+      //output_file_vec[chunk_cnt].open(output_fn);
+      *output_file_vec[chunk_cnt] << header << endl;
+      make_new_chunk = false;
+    }
+    
+    // if we are not grouping by a primary key, add the row to the 
+    // current ofstream
+    if (col_idx == -1){
+      *output_file_vec[chunk_cnt] << line << endl;
+      ++row_cnt;
+
+    // if we are grouping by a primary key, see which ofstream this 
+    // row belongs to and add it there
+    } else { 
+      this_key = get_entry_by_index(line, col_idx, ',');
+      if (keys_to_chunks.count(this_key) == 0){
+        keys_to_chunks[this_key] = chunk_cnt;
+      } 
+      if (keys_to_chunks[this_key] == chunk_cnt){
+        ++row_cnt;
+      }
+      *output_file_vec[keys_to_chunks[this_key]] << line << endl;
+    }
+
+    if (row_cnt >= rows_per_chunk-1){
+      make_new_chunk = true;
       ++chunk_cnt;
       row_cnt = 0;
-      output_file.close();
-    } else {
-      row_cnt += 1;
     }
   }
+  
+  for (auto p : output_file_vec){
+    (*p).close();
+    delete p;
+    p = NULL;
+  }  
  
   input_file.close();
+
   return 0;
 }
